e73ab94e83a827fea1952966c7d5ede112aa977c
agsamkin <a.g.samkin@gmail.com>
2021-07-27 09:36:20 +0300

    Sqrt(x), Climbing Stairs

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int res = solution.mySqrt(1);
+        int res = solution.climbStairs(45);
+        System.out.println(res);
 
     }
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,54 @@
+import java.beans.IntrospectionException;
+import java.util.HashMap;
+
 public class Solution {
 
     public int mySqrt(int x) {
 
-        return 0;
+        int res = 0;
+
+        int min = 1;
+        int max = x%2 == 0 ? x/2 : (x/2)+1;
+
+        while (max>=min) {
+
+            int mid = (min+max)/2;
+
+            if (mid>Integer.MAX_VALUE/mid) {
+                max = mid-1;
+                continue;
+            }
+
+           int tmp = mid*mid;
+
+           if (tmp==x) {res = mid; break;}
+           if (tmp>x) {max = mid-1;}
+           if (tmp<x) {min = mid+1;}
+
+        }
+
+        if (res == 0) {
+            res = min-1;
+        }
+
+        return res;
+
+    }
+
+    private HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+    public int climbStairs(int n) {
+
+        if (n==1) {return 1;}
+        else if (n==2) {return 2;}
+        else if (hashMap.containsKey(n)) {
+            return hashMap.get(n);
+        }
+        else {
+            int tmp = climbStairs(n-1) + climbStairs(n-2);
+            hashMap.put(n, tmp);
+            return tmp;
+        }
 
     }
 
