3c8c826efc177cc3bff46a3411c34b0a1afda821
ags <a.g.samkin@gmail.com>
2023-08-06 22:01:11 +0300

    Add _875

---
+++ src/problems/_153.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class _153 {
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[] {3,4,5,1,2}));
+    }
+
+    public static int findMin(int[] nums) {
+        return 0;
+    }
+}
--- src/problems/_875.java
+++ src/problems/_875.java
@@ -6,7 +6,38 @@ public class _875 {
     }
 
     public static int minEatingSpeed(int[] piles, int h) {
+        int min_speed = 1;
+        int max_speed = 1;
 
-        return 0;
+        for (int i = 0; i < piles.length; i++) {
+            max_speed = Math.max(max_speed, piles[i]);
+        }
+
+        while (min_speed < max_speed) {
+            int mid_speed = (min_speed + max_speed) / 2;
+            if (check(piles, h, mid_speed)) {
+                max_speed = mid_speed;
+            } else {
+                min_speed = mid_speed + 1;
+            }
+        }
+
+        return min_speed;
+
+    }
+
+    private static boolean check(int[] piles, int total_h, int mid_speed) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            int pile_h = piles[i] / mid_speed;
+            hours += pile_h;
+            if (piles[i] % mid_speed != 0) {
+                hours++;
+            }
+        }
+        if (hours <= total_h) {
+            return true;
+        }
+        return false;
     }
 }
